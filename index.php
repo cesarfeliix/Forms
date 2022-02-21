626aca901178ba6bc2a498956b53fbaeecc6803f
cesarfeliix <feliixcesar@gmail.com>
2022-02-21 22:04:02 +0000

    first commit

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<?php
+// create PHP  
+// create a form
+
+// add input tags, with name, and type (text, password, submit)
+
+//create a entry.php registry for the form input values
+?>
+</body>
+</html>
